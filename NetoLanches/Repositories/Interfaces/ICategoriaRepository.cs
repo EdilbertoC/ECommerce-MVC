7714361f803cb985041bc562e7de4276c55a9a7c
Edilberto Neto <edilbertoneto980@gmail.com>
2025-01-23 15:12:57 -0300

    Criada interface ICategoriaRepository

---
+++ NetoLanches/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using NetoLanches.Models;
+
+namespace NetoLanches.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+
+    }
+}
