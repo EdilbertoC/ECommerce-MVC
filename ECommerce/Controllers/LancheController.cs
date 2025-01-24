a290c637e7b630f0b1f5502426e7b8c6f721bc80
Edilberto Neto <edilbertoneto980@gmail.com>
2025-01-24 18:05:24 -0300

    Criado controller LancheController

---
+++ ECommerce/Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
