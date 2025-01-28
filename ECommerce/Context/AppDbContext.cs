b8ae991a23f6c9236940bc32532539625f08a2a1
Edilberto Neto <edilbertoneto980@gmail.com>
2025-01-28 14:17:38 -0300

    Criada classe CarrinhoCompraItem

--- ECommerce/Context/AppDbContext.cs
+++ ECommerce/Context/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace ECommerce.Context
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
     }
 }
--- ECommerce/Migrations/20250128171348_CarrinhoCompraItem.Designer.cs
+++ ECommerce/Migrations/20250128171348_CarrinhoCompraItem.Designer.cs
@@ -3,6 +3,7 @@ using ECommerce.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerce.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250128171348_CarrinhoCompraItem")]
+    partial class CarrinhoCompraItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
