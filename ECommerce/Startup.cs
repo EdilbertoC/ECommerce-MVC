def017cb7d9bb4a4121307ee6891a16f63218acd
Edilberto Neto <edilbertoneto980@gmail.com>
2025-02-05 14:35:32 -0300

    Criado e implementado ViewComponent CategoriaMenu

--- ECommerce/Startup.cs
+++ ECommerce/Startup.cs
@@ -61,7 +61,6 @@ public class Startup
                 pattern: "Lanche/{action}/{categoria?}",
                 defaults: new { controller = "Lanche", Action = "List" });
 
-                
             endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
