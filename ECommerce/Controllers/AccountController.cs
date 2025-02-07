d83ce0bc2a851d306b0fede8d55c76cc06408e83
Edilberto Neto <edilbertoneto980@gmail.com>
2025-02-07 10:46:36 -0300

    Adicionados métodos Register em AccountController

--- ECommerce/Controllers/AccountController.cs
+++ ECommerce/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ECommerce.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ECommerce.Controllers
 {
@@ -12,5 +14,66 @@ namespace ECommerce.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-    }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnURL = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVM.ReturnURL))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginVM.ReturnURL);
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+            return View(loginVM);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroVM.UserName };
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar usuário");
+                }
+            }
+            return View(registroVM);
+        }
+    } 
 }
