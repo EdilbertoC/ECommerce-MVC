8c54629e9f69efbc7c5af6ee005ee3c03df1210c
Edilberto Neto <edilbertoneto980@gmail.com>
2025-02-14 09:42:50 -0300

    Criado método logout em AccountController

--- ECommerce/Controllers/AccountController.cs
+++ ECommerce/Controllers/AccountController.cs
@@ -75,5 +75,13 @@ namespace ECommerce.Controllers
             }
             return View(registroVM);
         }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
